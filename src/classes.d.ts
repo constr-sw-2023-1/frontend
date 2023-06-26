b9b8939014fbfc17df28d21d8751f44d99afe993
vitordelela <vitordelela@gmail.com>
2023-06-26 20:37:21 -0300

    adding classes.d.ts

---
+++ src/classes.d.ts
@@ -0,0 +1,9 @@
+declare module '@pages/PrivatePages/Classes' {
+    const Classes: React.ComponentType<any>;
+    export default Classes;
+  }
+  
+  declare module '@pages/PrivatePages/Shifts' {
+    const Shifts: React.ComponentType<any>;
+    export default Shifts;
+  }
