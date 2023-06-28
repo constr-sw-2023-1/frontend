b8f184fed3d53d6a163e3700270d446d5862c4af
leonardo.mello <leonardo.mello@totvs.com.br>
2023-06-28 17:39:30 -0300

    criando tela

---
+++ src/pages/PrivatePages/Rooms/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import './Rooms.scss';
+
+const Rooms: React.FC = () => {
+    const [linguagem1, setLinguagem1] = useState('');
+    const [linguagem2, setLinguagem2] = useState('');
+    const [linguagem3, setLinguagem3] = useState('');
+    const [linguagem4, setLinguagem4] = useState('');
+
+    const handleLinguagem1Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLinguagem1(event.target.value);
+    };
+
+    const handleLinguagem2Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLinguagem2(event.target.value);
+    };
+
+    const handleLinguagem3Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLinguagem3(event.target.value);
+    };
+
+    const handleLinguagem4Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLinguagem4(event.target.value);
+    };
+
+    return (
+        <div className="rooms-container">
+
+            <h1>Criar/Editar sala</h1>
+
+
+            <select value={linguagem1} onChange={handleLinguagem1Change}>
+                <option value="" disabled hidden>Número da sala</option>
+                <option value="210" label="210" />
+                <option value="211" label="211" />
+                <option value="212" label="212" />
+                <option value="213" label="213" />
+            </select>
+
+            <select value={linguagem2} onChange={handleLinguagem2Change}>
+                <option value="" disabled hidden>Capacidade máxima</option>
+                <option value="minus40"> Menos que 40</option>
+                <option value="between4050">Entre 40 e 50</option>
+                <option value="between5060">Entre 50 e 60</option>
+                <option value="more60">Mais que 60</option>
+            </select>
+
+            <select value={linguagem3} onChange={handleLinguagem3Change}>
+                <option value="" disabled hidden>Tipo</option>
+                <option value="Normal">Normal</option>
+                <option value="Laboratório">Laboratório</option>
+                <option value="Auditório">Auditório</option>
+            </select>
+
+
+            <select value={linguagem4} onChange={handleLinguagem4Change}>
+                <option value="" disabled hidden>Prédio</option>
+                <option value="Prédio 1">Prédio 1</option>
+                <option value="Prédio 2">Prédio 2</option>
+                <option value="Prédio 3">Prédio 3</option>
+                <option value="Prédio 4">Prédio 4</option>
+            </select>
+        </div>
+    );
+};
+
+export default Rooms;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,9 @@ import Dashboard from "@pages/Dashboard";
 import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PrivatePages from "@pages/PrivatePages";
-import Classes from "@pages/PrivatePages/Classes"
+// import Classes from "@pages/PrivatePages/Classes"
+import Rooms from "@pages/PrivatePages/Rooms";
+
 
 export default function RoutesComponent() {
   return (
@@ -11,7 +13,8 @@ export default function RoutesComponent() {
         <Route path={""} element={<PrivatePages />}>
           {/* <Route path={""} element={<Dashboard />} /> */}
         </Route>
-        <Route path="/Turmas" element={<Classes/>}/>
+        {/* <Route path="/Turmas" element={<Classes />} /> */}
+        <Route path="/salasInitial" element={<Rooms />} />
       </Route>
     </Routes>
   );
