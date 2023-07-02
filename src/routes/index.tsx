4d2635d35a1845c732853a1c35e9b5ae5fbcd75f
Leonardo Berlatto <36887738+LeonardoBerlatto@users.noreply.github.com>
2023-07-02 16:49:15 -0300

    G4 Adding login screen
    * Adding global styles
    
    * Adding PUCRS logo
    
    * Adding auth service for login
    
    * Adding login screen
    
    * Renaming function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PrivatePages from "@pages/PrivatePages";
 import Classes from "@pages/PrivatePages/Classes"
+import Login from "@pages/Login";
 
 export default function RoutesComponent() {
   return (
@@ -12,6 +13,7 @@ export default function RoutesComponent() {
           {/* <Route path={""} element={<Dashboard />} /> */}
         </Route>
         <Route path="/Turmas" element={<Classes/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Route>
     </Routes>
   );
