b1b6fb788b66ca2e1457e39e209940c6b81fded5
Henrique Oliveira <h.oliveira@edu.pucrs.br>
2023-07-02 17:00:14 -0300

    Fixed theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./theme";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
