d7c9d2c659a161f9ca741007fe4d7f6e9ac818c0
Henrique Derlam Zwetsch <henrique.derlam@hp.com>
2023-07-05 11:12:24 -0300

    Add Rooms route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,6 +14,7 @@ import CreateCertification from "@pages/PrivatePages/Certifications/CreateCertif
 import Classes from "@pages/PrivatePages/Classes"
 import CreateEdit from "@pages/PrivatePages/Classes/CreateEdit/CreateEdit"
 import Login from "@pages/Login";
+import Rooms from "@pages/PrivatePages/Rooms";
 
 export default function RoutesComponent() {
   return (
@@ -39,6 +40,7 @@ export default function RoutesComponent() {
         <Route path="/Periodos" element={<Shifts />} />
         <Route path="/create-shift" element={<CreateShiftsPage name={""} />} />
         <Route path="/edit-shift/:id" element={<CreateShiftsPage name={""} />} />
+        <Route path="/salasInitial" element={<Rooms />} />
       </Route>
     </Routes>
   );
